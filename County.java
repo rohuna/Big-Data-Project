dd5ce64333e518de423dc010fb31d1abde0b983d
rohuna <agrawal.rohun@gmail.com>
2019-04-04 19:42:48 -0700

    Update County.java

--- County.java
+++ County.java
@@ -1,55 +1,52 @@
 public class County
 {  
-    String name;
-    String state;
-    String hsDiploma2012;
-    String avgUnemployment;
+    private String name; //name of the county
+    private String state; //name of the state of the county
+    private String hsDiploma2012; //average rate of high school education between 2012-2016
+    private String avgUnemployment; //average rate of unemployment between 2012-2016
+    private double ba; //total African American population 
+    private double wa; //total White population
 
-    public County(String state, String name, String hsDiploma2012, String avgUnemployment)
+    /** Constructor **/
+    public County(String state, String name, String hsDiploma2012, String avgUnemployment, double wa, double ba)
     {
         this.name = name;
         this.state = state;
         this.hsDiploma2012 = hsDiploma2012;
         this.avgUnemployment = avgUnemployment;
+        this.wa = wa;
+        this.ba = ba;
     }
 
-    public String getState()
-    {
-        return state;
-    }
+    /** Accessor Methods **/
+    public String getState() { return state; }
 
-    public String getName()
-    {
-        return name;
-    }
+    public String getName() { return name; }
 
-    public String getHsDiploma2012()
-    {
-        return hsDiploma2012;
-    }
+    public String getHsDiploma2012() { return hsDiploma2012; }
 
-    public String getAvgUnemployment()
-    {
-        return avgUnemployment;
-    }
+    public String getAvgUnemployment() { return avgUnemployment; }
 
+    public double getB() { return ba; }
+
+    public double getW(); { return wa; }
+
+    /** Setter Methods **/
     public void setState(String state)
-    {
-        this.state = state;
-    }
+    { this.state = state; }
 
     public void setName(String name)
-    {
-        this.name = name;
-    }
+    { this.name = name; }
 
     public void setHsDiploma2012(String hsDiploma2012)
-    {
-        this.hsDiploma2012 = hsDiploma2012;
-    }
+    { this.hsDiploma2012 = hsDiploma2012; }
 
     public void setAvgUnemployment(String avgUnemployment)
-    {
-        this.avgUnemployment = avgUnemployment;
-    }
+    { this.avgUnemployment = avgUnemployment; }
+
+    public void setB(double ba)
+    { this.ba = ba; }
+
+    public void setW(double wa)
+    { this.wa = wa; }
 }
